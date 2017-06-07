3d3f6b1c4a87bb09bc0a37d4fcc6f7b99a7dd6e1
Evan Bleiweiss <hello@awst.in>
2017-06-06 21:39:09 -0500

    ohhh... hello

--- functional-javascript-workshop/exercise1.js
+++ functional-javascript-workshop/exercise1.js
@@ -1,28 +1,33 @@
- Hello World
- Exercise 1 of 18
+ // Hello World
+ // Exercise 1 of 18
 
+// # Task
 
-# Task
+// Write a function that takes an input string and returns it uppercased.
 
-Write a function that takes an input string and returns it uppercased.
+// ## Arguments
 
-## Arguments
+//   * input: a String of random words (lorem ipsum).
 
-  * input: a String of random words (lorem ipsum).
+// ## Boilerplate
 
-## Boilerplate
-
-    function upperCaser(input) {
-      // SOLUTION GOES HERE
-    }
+//     function upperCaser(input) {
+//       // SOLUTION GOES HERE
+//     }
     
-    module.exports = upperCaser
+//     module.exports = upperCaser
 
 
- » To print these instructions again, run: functional-javascript print
- » To execute your program in a test environment, run: functional-javascript run program.js
- » To verify your program, run: functional-javascript verify program.js
- » For help run: functional-javascript help
+//  » To print these instructions again, run: functional-javascript print
+//  » To execute your program in a test environment, run: functional-javascript run program.js
+//  » To verify your program, run: functional-javascript verify program.js
+//  » For help run: functional-javascript help
 
 
+function upperCaser(input) {
+    if (typeof(input) === "string") {
+        return input.toUpperCase();
+    }
+}
 
+module.exports = upperCaser
